c95718e1e225911f82a705f4d60461fef52e0b15
Mikael Grön <skaramicke@gmail.com>
2023-07-26 12:42:41 +0200

    Fix nicer footer with content

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { CmsImage, CmsText } from "../types/cms";
 import Content from "./Content";
 
@@ -6,14 +6,47 @@ type FooterProps = {
   items: (CmsText | CmsImage)[];
 };
 
-const Footer: React.FC<FooterProps> = ({ items }) => (
-  <footer className="container px-8 mx-auto xl:px-5  max-w-screen-lg py-5 lg:py-8 bg-lime-100 border-l border-r">
-    <div className="flex flex-wrap justify-between gap-1">
-      {items.map((item, i) => (
-        <Content key={i} data={item} depth={0} />
-      ))}
-    </div>
-  </footer>
-);
+const Footer: React.FC<FooterProps> = ({ items }) => {
+  const [gridStyle, setGridStyle] = React.useState({} as any);
+
+  useEffect(() => {
+    if (!items.length || items.length < 1) return;
+    switch (items.length) {
+      case 1:
+        setGridStyle("grid grid-cols-1 gap-4");
+        break;
+
+      case 2:
+        setGridStyle("grid grid-cols-2 gap-4");
+        break;
+
+      case 3:
+        setGridStyle("grid grid-cols-3 gap-4");
+        break;
+
+      case 4:
+        setGridStyle("grid grid-cols-4 gap-4");
+        break;
+
+      case 5:
+        setGridStyle("grid grid-cols-3 gap-4");
+        break;
+      default:
+        break;
+    }
+  }, [items.length]);
+
+  if (!items.length || items.length < 1) return null;
+
+  return (
+    <footer className="container px-8 mx-auto xl:px-5  max-w-screen-lg py-5 lg:py-8 bg-lime-100 border-l border-r">
+      <div className={gridStyle}>
+        {items.map((item, i) => (
+          <Content key={i} data={item} depth={0} />
+        ))}
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
